371b11027485584ae528435a062313b3e49e5d04
Michel Anderson <michelaljunior@outlook.com>
2023-09-30 15:55:19 -0300

    Alteração de string de conexão para o banco local

--- ApiTeste.Persistence/Context/BancoApiTesteContext.cs
+++ ApiTeste.Persistence/Context/BancoApiTesteContext.cs
@@ -22,10 +22,6 @@ public partial class BancoApiTesteContext : DbContext
 
     public virtual DbSet<TblUsuario> TblUsuarios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=bancoApiTeste;Integrated Security=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<TblLivro>(entity =>
